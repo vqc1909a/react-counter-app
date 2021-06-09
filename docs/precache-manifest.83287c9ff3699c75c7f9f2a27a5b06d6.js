bd1659a5517f6ed33e0fd46b75e30fb7961e1ee4
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2021-06-09 11:22:13 -0500

    Docs agregado

---
+++ docs/precache-manifest.83287c9ff3699c75c7f9f2a27a5b06d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab38270dc48f5bbaffbaaaa83978528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec2cd08773f366e07fd",
+    "url": "/static/css/main.a6601f16.chunk.css"
+  },
+  {
+    "revision": "b8deebddd17c2bfdc50a",
+    "url": "/static/js/2.ee48d043.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ee48d043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec2cd08773f366e07fd",
+    "url": "/static/js/main.e6918c4d.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
